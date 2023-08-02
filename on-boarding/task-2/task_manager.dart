9bdb661c46e4f3abd7c92650aa59dcef51d318c8
Gedion Ezra <gediezra@gmail.com>
2023-08-02 17:30:56 +0300

    minor fixes

--- on-boarding/task-2/task_manager.dart
+++ on-boarding/task-2/task_manager.dart
@@ -33,23 +33,31 @@ class TaskManager {
   }
 
   void viewCompletedTasks() {
-    print("-----------------------------------------------------");
+    int _completedTasksCount = 0;
     if (taskList.isEmpty) {
       print("No completed tasks yet");
       print('----------------------------------------------------');
       return;
     }
+    print("-----------------------------------------------------");
     for (Tasks task in taskList) {
       if (task.taskStatus == TaskStatus.Completed) {
         print("Title: ${task.title}");
         print("Task Description: ${task.description}");
         print("Task is due: ${task.time.toString()}");
         print("-------------------------------------------------");
+        _completedTasksCount++;
       }
+    }
+    if (_completedTasksCount == 0 || taskList.isEmpty) {
+      print("No completed tasks yet");
+      print('----------------------------------------------------');
+      return;
     }
   }
 
   void viewPendingTasks() {
+    int _pendingTasksCount = 0;
     print("------------------------------------------------------");
     if (taskList.isEmpty) {
       print("No pending tasks yet");
@@ -62,7 +70,13 @@ class TaskManager {
         print("Task Description: ${task.description}");
         print("Task is due: ${task.time.toString()}");
         print("-------------------------------------------------");
+        _pendingTasksCount++;
       }
+    }
+    if (_pendingTasksCount == 0) {
+      print("No pending tasks yet");
+      print('----------------------------------------------------');
+      return;
     }
   }
 
@@ -74,11 +88,13 @@ class TaskManager {
     taskList[index].time = time;
 
     print("Task ${index + 1} edited succesfully");
+    print('----------------------------------------------------');
   }
 
   void DeleteTask(int index) {
     taskList.removeAt(index);
     print("Task successfully deleted");
+      print('----------------------------------------------------');
   }
 
   void printTask() {
